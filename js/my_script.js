a85dec4dc2b7684c9ec411646e533ee4151c1c80
MorenaFischione <fischione.morena@gmail.com>
2021-09-14 18:24:57 +0200

    funzione che cambia il colore all' <h1>  => "Benvenuto"

--- js/my_script.js
+++ js/my_script.js
@@ -4,15 +4,25 @@
 var app = new Vue ({
         
     el: "#root",
+    // tutte le variabili da monitorare da HTML
     data: {
+        title: "Buongiorno",
         message: "Benvenuto ",
         nome: "",
         cognome: "",
         img: "https://i2.wp.com/www.blogparsec.it/wp-content/uploads/2020/08/homer-758x550-1.jpg?resize=406%2C294",
+        mieClassi: "pt-5 text-danger ",
+        color: "",
+    },
+    methods: {
+        changeColor: function () {
+            if(this.color == "magenta"){
+                this.color = "blue";
+            } else {
+                this.color = "magenta";
+            }
+        }
     }
-    // methods: {
-
-    // }
     
 });
 
