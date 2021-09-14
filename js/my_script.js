5a17ce4fe6bcb2f102f2205ead2298daacf9fed4
MorenaFischione <fischione.morena@gmail.com>
2021-09-14 16:21:05 +0200

    aggiunta di immagine e di input

--- js/my_script.js
+++ js/my_script.js
@@ -1,12 +1,18 @@
 
-// istanzizzione di Vue
+// istanzizzione di Vue. Creo un nuovo oggetto
 
 var app = new Vue ({
         
     el: "#root",
     data: {
-        message: "Buongiorno"
+        message: "Benvenuto ",
+        nome: "",
+        cognome: "",
+        img: "https://i2.wp.com/www.blogparsec.it/wp-content/uploads/2020/08/homer-758x550-1.jpg?resize=406%2C294",
     }
+    // methods: {
+
+    // }
     
 });
 
